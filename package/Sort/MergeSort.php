efaab21e997543ecf1e166600f783635c2f2f095
pushaowei <542684913@qq.com>
2017-08-23 19:21:31 +0800

    fix: Increase the process; :rocket:

--- package/Sort/MergeSort.php
+++ package/Sort/MergeSort.php
@@ -60,6 +60,7 @@ class MergeSort
      */
     public function mergeArray(&$arr, $left, $center, $right)
     {
+        echo '| '.$left.' - '.$center.' - '.$right.' - '.implode(',',$arr).PHP_EOL;
         //设置两个起始位置标记
         $a_i = $left;
         $b_i = $center + 1;
@@ -92,4 +93,29 @@ class MergeSort
 //do some test:
 new mergeSort([4, 7, 6, 3, 9, 5, 8]);
 
+/*
 
+| 0 - 0 - 1 - 4,7,6,3,9,5,8
+| 2 - 2 - 3 - 4,7,6,3,9,5,8
+| 0 - 1 - 3 - 4,7,3,6,9,5,8
+| 4 - 4 - 5 - 3,4,6,7,9,5,8
+| 4 - 5 - 6 - 3,4,6,7,5,9,8
+| 0 - 3 - 6 - 3,4,6,7,5,8,9
+
+array(7) {
+  [0] =>
+  int(3)
+  [1] =>
+  int(4)
+  [2] =>
+  int(5)
+  [3] =>
+  int(6)
+  [4] =>
+  int(7)
+  [5] =>
+  int(8)
+  [6] =>
+  int(9)
+}
+ */
