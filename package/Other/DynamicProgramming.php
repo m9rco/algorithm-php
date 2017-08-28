144107918f590c2691e91f496b513b11b457b411
pushaowei <542684913@qq.com>
2017-08-28 09:47:53 +0800

    feat: add DynamicProgramming :construction_worker:

---
+++ package/Other/DynamicProgramming.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * DynamicProgramming
+ *
+ * @author   Pu ShaoWei <marco0727@gamil.com>
+ * @date     2017/8/28
+ * @license  Mozilla
+ * -------------------------------------------------------------
+ * 思路分析：
+ * -------------------------------------------------------------
+ * 题目放在这里，9点来完成实例
+ * 
+ * @param $n
+ * @return int
+ */
+function DynamicProgramming( $str )
+{
+
+}
+
+
