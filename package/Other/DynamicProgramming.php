f81f5a2c45509e5a13c43a8956a814a2b9127e8e
pushaowei <542684913@qq.com>
2017-09-02 15:53:15 +0800

    feat: ColorBricks :green_heart:

--- package/Other/DynamicProgramming.php
+++ package/Other/DynamicProgramming.php
@@ -11,12 +11,11 @@
  * -------------------------------------------------------------
  * 一个承受最大重量为W的背包，现在有n个物品，每个物品重量为t, 每个物品的价值为v。
  * 要使得这个背包重量最大(但不能超过W),同时又需要背包的价值最大
- *
  * @return int
  */
-function DynamicProgramming( $maxSize ,$goods, $cost)
+function DynamicProgramming($maxSize, $goods, $cost)
 {
-    $container = [];
+    $container  = [];
     $goodsCount = count($goods);
 
     //初始化
@@ -31,19 +30,19 @@ function DynamicProgramming( $maxSize ,$goods, $cost)
             $container[$j][$i] = $container[$j - 1][$i];
             //不大于最大的w=15
             if ($goods[$j - 1] <= $maxSize) {
-                if (!isset($container[ $j - 1 ][ $i - $goods[ $j - 1 ] ])) {
+                if (!isset($container[$j - 1][$i - $goods[$j - 1]])) {
                     continue;
                 }
-            //wi+opt(i-1,wi)
-                $tmp = $container[ $j - 1 ][ $i - $goods[ $j - 1 ] ] + $cost[ $j - 1 ];
-            //opt(i-1,w),wi+opt(i-1,w-wi) => 进行比较
+                //wi+opt(i-1,wi)
+                $tmp = $container[$j - 1][$i - $goods[$j - 1]] + $cost[$j - 1];
+                //opt(i-1,w),wi+opt(i-1,w-wi) => 进行比较
                 if ($tmp > $container[$j][$i]) {
                     $container[$j][$i] = $tmp;
                 }
             }
         }
     }
-    var_dump($container[$j-1][$i - 1]);
+    var_dump($container[$j - 1][$i - 1]);
 }
 
 echo DynamicProgramming(15, [3, 4, 5, 6], [8, 7, 4, 9]);
