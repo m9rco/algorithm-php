bce8e826bf3bbceb5a9b062ffebb2d5cc2b5625d
pushaowei <542684913@qq.com>
2017-09-22 15:30:46 +0800

    fix: modify bug :sparkles:

--- package/Sort/ShellSort.php
+++ package/Sort/ShellSort.php
@@ -9,18 +9,17 @@
  * -------------------------------------------------------------
  * 希尔排序中一个常数因子n，原数组被分成各个小组，每个小组由h个元素组成，很可能会有多余的元素。
  * 当然每次循环的时候，h也是递减的（h=h/n）。第一次循环就是从下标为h开始。希尔排序的一个思想就是，分成小组去排序。
- *
  * @param array $container
  * @return array
  */
-function ShellSort( array $container )
+function ShellSort(array $container)
 {
     $count = count($container);
-    for ( $increment = intval($count / 2); $increment > 0; $increment = intval($increment / 2) ) {
-        for ( $i = $increment; $i < $count; $i++ ) {
+    for ($increment = intval($count / 2); $increment > 0; $increment = intval($increment / 2)) {
+        for ($i = $increment; $i < $count; $i++) {
             $temp = $container[$i];
-            for ( $j = $i; $j >= $increment; $j -= $increment ) {
-                if ( $temp < $container[$j - $increment] ) {
+            for ($j = $i; $j >= $increment; $j -= $increment) {
+                if ($temp < $container[$j - $increment]) {
                     $container[$j] = $container[$j - $increment];
                 } else {
                     break;
@@ -32,5 +31,6 @@ function ShellSort( array $container )
     return $container;
 }
 
-var_dump( ShellSort([6,13,21,99,18,2,25,33,19,84]));
+//var_dump(ShellSort([6, 13, 21, 99, 18, 2, 25, 33, 19, 84]));
+
 
