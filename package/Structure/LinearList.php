1965544d03473c50aea68ebdf730a37e78509b50
pushaowei <542684913@qq.com>
2017-09-30 12:41:44 +0800

    fix: Fixed redundancy :construction_worker:

--- package/Structure/LinearList.php
+++ package/Structure/LinearList.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------
  * [线性表顺序存储]
  * =======
- * 把线性表的结点按逻辑顺序依次存放在一组地址连续的存储单元里，用这种方法存储的线性表简称顺序表。
+ * 把线性表的结点按逻辑顺序依次存放在一组地址连续的存储单元里，用这种方法存储的线性表简称线性表。
  * -------------------------------------------------------------
  * [顺序存储的线性表的特点]
  * =======
@@ -37,164 +37,181 @@
  * -------------------------------------------------------------
  * @param array
  */
-class LinearList
+class LinearList extends ArrayObject
 {
-    const PRECURSOR_KEY   = 0;
-    const PRECURSOR_VALUE = 1;
+    const PRECURSOR_KEY    = 0;
+    const PRECURSOR_VALUE  = 1;
 
     const SUBSEQUENT_KEY   = 0;
     const SUBSEQUENT_VALUE = 1;
 
-    const ASSIGN_KEY  = 0;
-    const DEFAULT_KEY = 1;
+    const ASSIGN_KEY       = 0;
+    const DEFAULT_KEY      = 1;
 
+    const DELETE_KEY       = 0;
+    const DELETE_VALUE     = 1;
 
     /**
-     * @var array|mixed 顺序表
+     * @var array|mixed 线性表
      */
     public $oll;
 
     /**
-     * LinearList constructor.  顺序表初始化
+     * LinearList constructor.  线性表初始化
      *
      * @param array $oll
      */
     public function __construct($oll = array ())
     {
-        $this->oll = $oll;
+        echo '---------------------------'.PHP_EOL;
+        echo var_export($oll).PHP_EOL;
+        echo '---------------------------'.PHP_EOL;
+        parent::__construct($oll);
+        $this->oll = $this->getIterator();
     }
 
     /**
-     * @return void 清空顺序表
+     * @return void 清空线性表
      */
     public function __destruct()
     {
-        $this->preDispatch(); // 调试用，请无视
-        foreach ($this->oll as $key => $value) {
-            unset($this->oll[$key]);
-        }
+        unset($this->oll);
     }
 
     /**
-     * 判断顺序表是否为空
+     * 调试用请无视
+     *
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        $this->preDispatch(); // 调试用，请无视
+        return call_user_func_array(array($this, $name), $arguments);
+    }
+
+    /**
+     * 判断线性表是否为空
      *
      * @return boolean 为空返回true,否则返回false
      */
-    public function isEmpty()
+    protected function isEmpty()
     {
-        $this->preDispatch(); // 调试用，请无视
-        return count($this->oll) > 0 ? false : true;
+        return $this->getLength() > 0 ? false : true;
     }
 
     /**
-     * 返回顺序表的长度
+     * 返回线性表的长度
      *
      * @return int
      */
-    public function getLength()
+    protected function getLength()
     {
-        $this->preDispatch(); // 调试用，请无视
-        return count($this->oll);
+        return $this->oll->count();
     }
 
     /**
-     * 返回顺序表中下标为$key的元素
+     * 返回线性表中下标为$key的元素
      *
-     * @param mixed $key 顺序表元素的下标
+     * @param mixed $key 线性表元素的下标
      * @return mixed
      */
-    public function getElement($key)
+    protected function getElement($key)
     {
-        $this->preDispatch(); // 调试用，请无视
-        return $this->oll[$key];
+        return $this->oll->offsetGet($key);
     }
 
     /**
-     * 返回顺序表中某个元素的位置
+     * 返回线性表中某个元素的位置
      *
-     * @param mixed $value 顺序表中某个元素的值
+     * @param mixed $value 线性表中某个元素的值
      * @return int 从1开始,如果返回-1表示不存在该元素
      */
-    public function getElementPosition($value)
+    protected function getElementPosition($value)
     {
-        $this->preDispatch(); // 调试用，请无视
         $i = 0;
-        foreach ($this->oll as $val) {
+        $this->oll->rewind();
+        while ($this->oll->valid()) {
             $i++;
-            if (strcmp($value, $val) === 0) {
+            if (strcmp($value, $this->oll->current()) === 0) {
                 return $i;
             }
+            $this->oll-> next();
         }
         return -1;
     }
 
     /**
-     * 返回顺序表中某个元素的直接前驱元素
+     * 返回线性表中某个元素的直接前驱元素
      *
-     * @param string $value 顺序表中某个元素的值
+     * @param string $value 线性表中某个元素的值
      * @param int    $tag   如果$value为下标则为1, 如果$value为元素值则为0
      * @return array|bool        array('value'=>...)直接前驱元素值，array('key'=>...)直接前驱元素下标
      */
-    public function getElementPrecursor($value, $tag = self::PRECURSOR_VALUE)
+    protected function getElementPrecursor($value, $tag = self::PRECURSOR_VALUE)
     {
-        $this->preDispatch(); // 调试用，请无视
-        $i = 0;
-        foreach ($this->oll as $key => $val) {
-            $i++;
+        $i    = 0;
+        $prev = null;
+        $this->oll->rewind();
+        while ($this->oll->valid()) {
+            $key     = $this->oll->key();
+            $current = $this->oll->current();
+            if (strcmp($key, $value) === 0) {
+                if ($i == 1) {
+                    return false;
+                }
+                return array ('value' => $this->getElement($prev), 'key' => $prev);
+            }
             if ($tag == self::PRECURSOR_VALUE) {
-                if (strcmp($key, $value) === 0) {
+                if (strcmp($current, $value) === 0) {
                     if ($i == 1) {
                         return false;
                     }
-                    prev($this->oll);
-                    prev($this->oll);
-                    return array ('value' => current($this->oll), 'key' => key($this->oll));
+                    return array ('value' => $this->getElement($prev), 'key' => $prev);
                 }
             }
-
-            if ($tag == self::PRECURSOR_KEY) {
-                if (strcmp($val, $value) === 0) {
-                    if ($i == 1) {
-                        return false;
-                    }
-                    prev($this->oll);
-                    prev($this->oll);
-                    return array ('value' => current($this->oll), 'key' => key($this->oll));
-                }
-            }
+            $i++;
+            $prev = $this->oll->key();
+            $this->oll->next();
         }
     }
 
     /**
      * 返回某个元素的直接后继元素
      *
-     * @param     $value $value顺序表中某个元素的值
+     * @param string   $value $value线性表中某个元素的值
      * @param int $tag   如果$value为下标则为1,如果$value为元素值则为0
      * @return array|bool       array('value'=>...)直接后继元素值，array('key'=>...)直接后继元素下标
      */
-    public function getElementSubsequent($value, $tag = self::SUBSEQUENT_KEY)
+    protected function getElementSubsequent($value, $tag = self::SUBSEQUENT_KEY)
     {
-        $this->preDispatch(); // 调试用，请无视
         $i   = 0;
-        $len = count($this->oll);
-        foreach ($this->oll as $key => $val) {
-            $i++;
+        $len = $this->getLength();
+        $this->oll->rewind();
+        while ($this->oll->valid()) {
+            $key     = $this->oll->key();
+            $current = $this->oll->current();
             if ($tag == self::SUBSEQUENT_KEY) {
                 if (strcmp($key, $value) == 0) {
                     if ($i == $len) {
                         return false;
                     }
-                    return array ('value' => current($this->oll), 'key' => key($this->oll));
+                    $this->oll->next();
+                    return array ('value' => $this->oll->current(), 'key' => $this->oll->key());
                 }
             }
             if ($tag == self::SUBSEQUENT_VALUE) {
-                if (strcmp($val, $value) == 0) {
+                if (strcmp($current, $value) == 0) {
                     if ($i == $len) {
                         return false;
                     }
-                    return array ('value' => current($this->oll), 'key' => key($this->oll));
+                    $this->oll->next();
+                    return array ('value' => $this->oll->current(), 'key' => $this->oll->key());
                 }
             }
+            $i++;
+            $this->oll->next();
         }
         return false;
     }
@@ -202,188 +219,112 @@ class LinearList
     /**
      * 在指定位置插入一个新的结点
      *
-     * @param string $p     新结点插入位置,从1开始
-     * @param string $value 顺序表新结点的值
-     * @param null   $key   顺序表新结点的下标
+     * @param string $p     新结点插入位置,从0开始
+     * @param string $value 线性表新结点的值
+     * @param null   $key   线性表新结点的下标
      * @param int    $tag   是否指定新结点的下标,1表示默认下标,0表示指定下标
-     * @return bool        插入成功返回true，失败返回false
+     * @return bool         插入成功返回true，失败返回false
      */
-    public function getInsertElement($p, $value, $key = null, $tag = self::DEFAULT_KEY)
+    protected function getInsertElement($p, $value, $key = null, $tag = self::DEFAULT_KEY)
     {
-        $this->preDispatch(); // 调试用，请无视
-
         $p   = (int)$p;
-        $len = count($this->oll);
-        $oll = array ();
         $i   = 0;
-        if ($p > $len || $p < 1) {
+        if ($p > $this->getLength() || $p < 1) {
             return false;
         }
-
-        foreach ($this->oll as $k => $v) {
-            $i++;
-            if ($i == (int)$p) {
-                if ($tag == self::DEFAULT_KEY) {
-                    $oll[] = $value;
-                } else if ($tag == self::ASSIGN_KEY) {
-                    $keys = array_keys($oll);
-                    $j    = 0;
-                    if (is_int($key)) {
-                        while (in_array($key, $keys, true)) {
-                            $key++;
-                        }
-                    } else {
-                        while (in_array($key, $keys, true)) {
-                            $j++;
-                            $key .= (string)$j;
-                        }
-                    }
-                    $oll[$key] = $value;
-                } else {
-                    return false;
-                }
-
-                $key  = $k;
-                $j    = 0;
-                $keys = array_keys($oll);
-                if (is_int($key)) {
-                    $oll[] = $v;
-                } else {
-                    while (in_array($key, $keys, true)) {
-                        $j++;
-                        $key .= (string)$j;
-                    }
-                    $oll[$key] = $v;
-                }
-            } else {
-                if ($i > $p) {
-                    $key  = $k;
-                    $j    = 0;
-                    $keys = array_keys($oll);
-                    if (is_int($key)) {
-                        $oll[] = $v;
-                    } else {
-                        while (in_array($key, $keys, true)) {
-                            $j++;
-                            $key .= (string)$j;
-                        }
-                        $oll[$key] = $v;
-                    }
-                } else {
-                    if (is_int($k)) {
-                        $oll[] = $v;
-                    } else {
-                        $oll[$k] = $v;
-                    }
-                }
+        $this->oll->rewind();
+        while ($this->oll->valid()) {
+            if ($i != $p) {
+                $i++;
+                $this->oll->next();
+            }
+            switch ($tag){
+                case self::DEFAULT_KEY:
+                    $this->oll->append($value);
+                    break 2;
+                case self::ASSIGN_KEY:
+                    $this->oll->offsetSet($key, $value);
+                    break 2;
             }
         }
-        $this->oll = $oll;
         return true;
     }
 
     /**
-     * 根据元素位置返回顺序表中的某个元素
+     * 根据元素位置返回线性表中的某个元素
      *
      * @param mixed $position 元素位置从1开始
      * @return array|bool  array('value'=>...)元素值，array('key'=>...)元素下标
      */
-    public function getElemForPos($position)
+    protected function getElemForPos($position)
     {
-        $this->preDispatch(); // 调试用，请无视
-
         $i        = 0;
-        $len      = count($this->oll);
+        $len      = $this->getLength();
         $position = (int)$position;
-
         if ($position > $len || $position < 1) {
             return false;
         }
-
-        foreach ($this->oll as $value) {
-            $i++;
+        $this->oll->rewind();
+        while ($this->oll->valid()) {
             if ($i == $position) {
-                return array ('value' => current($this->oll), 'key' => key($this->oll));
+                return array ('value' => $this->oll->current(), 'key' => $this->oll->key());
             }
+            $i++;
+            $this->oll->next();
         }
     }
 
     /**
-     * 根据下标或者元素值删除顺序表中的某个元素
+     * 根据下标或者元素值删除线性表中的某个元素
      *
      * @param mixed $value 元素下标或者值
-     * @param int   $tag   1表示$value为下标，2表示$value为元素值
+     * @param int   $tag   0表示$value为下标，1表示$value为元素值
      * @return bool 成功返回true,失败返回false
      */
-    public function getDeleteElement($value, $tag = 1)
+    protected function getDeleteElement($value, $tag = self::DELETE_KEY)
     {
-        $this->preDispatch(); // 调试用，请无视
-
-        $len = count($this->oll);
-        $oll = array ();
-        foreach ($this->oll as $k => $v) {
-            if ($tag == 1) {
-                if (strcmp($k, $value) === 0) {
-                } else {
-                    if (is_int($k)) {
-                        $oll[] = $v;
-                    } else {
-                        $oll[$k] = $v;
-                    }
+        $this->oll->rewind();
+        while ($this->oll->valid())
+        {
+            $key     = $this->oll->key();
+            $current = $this->oll->current();
+            if ($tag ==  self::DELETE_KEY) {
+                if (strcmp($key, $value) === 0) {
+                    $this->oll->offsetUnset($key);
                 }
             }
-
-            if ($tag == 2) {
-                if (strcmp($v, $value) === 0) {
-                } else {
-                    if (is_int($k)) {
-                        $oll[] = $v;
-                    } else {
-                        $oll[$k] = $v;
-                    }
+            if ($tag == self::DELETE_VALUE) {
+                if (strcmp($current, $value) === 0) {
+                    $this->oll->offsetUnset($key);
                 }
             }
-        }
-        $this->oll = $oll;
-        if (count($this->oll) == $len) {
-            return false;
+            $this->oll->next();
         }
         return true;
     }
 
     /**
-     * 根据元素位置删除顺序表中的某个元素
+     * 根据元素位置删除线性表中的某个元素
      *
      * @param int $position 元素位置从1开始
      * @return bool 成功返回true,失败返回false
      */
-    public function getDeleteEleForPos($position)
+    protected function getDeleteEleForPos($position)
     {
-        $this->preDispatch(); // 调试用，请无视
-
-        $len      = count($this->oll);
+        $len      = $this->getLength();
         $i        = 0;
         $position = (int)$position;
-        $oll      = array ();
         if ($position > $len || $position < 1) {
             return false;
         }
-
-        foreach ($this->oll as $k => $v) {
-            $i++;
+        $this->oll->rewind();
+        while ($this->oll->valid()) {
+            $key     = $this->oll->key();
             if ($i == $position) {
-            } else {
-                if (is_int($k)) {
-                    $oll[] = $v;
-                } else {
-                    $oll[$k] = $v;
-                }
+                $this->oll->offsetUnset($key);
             }
-        }
-
-        $this->oll = $oll;
-        if (count($this->oll) == $len) {
-            return false;
+            $i++;
+            $this->oll->next();
         }
         return true;
     }
@@ -392,34 +333,34 @@ class LinearList
      * 调试用
      *
      * @param bool $isDebug
-     * @return mixed
+     * @return bool
      */
-    public function preDispatch($isDebug = true)
+    protected function preDispatch($isDebug = true)
     {
-        if(!$isDebug){
+        if (!$isDebug) {
             return false;
         }
-        $debug = debug_backtrace()[1];
-        $args  = isset($debug['args']) ? implode(',',$debug['args']) : "";
-        echo "{$debug['function']}({$args})\n".PHP_EOL;
+        $debug      = debug_backtrace()[1];
+        $reflection = new ReflectionMethod($this, $debug['args'][0]);
+        $args       = isset($debug['args'][1]) ? implode(',', $debug['args'][1]) : "";
+        echo $reflection->getDocComment() . PHP_EOL;
+        echo "{$debug['args'][0]}({$args})\n" . PHP_EOL;
     }
 }
 
-$echo = function () {
-    $args = func_get_args();
-    echo $args[0] . "\t->\t" . var_export($args[1], true) . PHP_EOL;
+$echo = function ($str, $action) {
+    echo $str . "\t->\t" . var_export($action, true) . PHP_EOL;
     echo "--------------------------- " . PHP_EOL;
 };
 
-$oll = new LinearList(array ('name' => 'Jack', 10, "age", 'msg' => 10, 455));
-$echo('判断顺序表是否为空,返回false说明不为空', $oll->isEmpty());
-$echo('返回顺序表的长度 返回6', $oll->getLength());
-$echo('根据下标返回顺序表中的某个元素', $oll->getElement(1));
-$echo('返回顺序表中某个元素的位置', $oll->getElementPosition(455));
-$echo('返回顺序表中某个元素的直接前驱元素', $oll->getElementPrecursor(455, 2));
-$echo('返回顺序表中某个元素的直接后继元素', $oll->getElementSubsequent(455, 2));
-$echo('根据元素位置返回顺序表中的某个元素', $oll->getElemForPos(2));
-$echo('根据下标或者元素值删除顺序表中的某个元素', $oll->getDeleteElement('name', $tag = 2));
-$echo('根据元素位置删除顺序表中的某个元素', $oll->getDeleteEleForPos(1));
-$echo('在指定位置插入一个新的结点', $oll->getInsertElement(3, "插入新节点", $key = "msg", $tag = 2));
+$oll = new LinearList(array ('name' => 'Jack', 10, "age", 'msg' => 10, 666));
+$echo('判断线性表是否为空', $oll->isEmpty());
+$echo('返回线性表的长度', $oll->getLength());
+$echo('根据下标返回线性表中的某个元素', $oll->getElement(1));
+$echo('返回线性表中某个元素的位置', $oll->getElementPosition(666));
+$echo('返回线性表中某个元素的直接前驱元素', $oll->getElementPrecursor(666, LinearList::PRECURSOR_VALUE));
+$echo('返回线性表中某个元素的直接后继元素', $oll->getElementSubsequent(0, LinearList::SUBSEQUENT_KEY));
+$echo('根据元素位置返回线性表中的某个元素', $oll->getElemForPos(2));
+$echo('根据下标或者元素值删除线性表中的某个元素', $oll->getDeleteElement('name', LinearList::DELETE_KEY));
+$echo('在指定位置插入一个新的结点', $oll->getInsertElement(3, "插入新节点", "qzone", LinearList::ASSIGN_KEY));
 $echo('$oll->oll的内容 ', $oll->oll);
